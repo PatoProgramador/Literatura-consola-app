d0cec07d4d3e5fbed32d55a48ab5e20ab07c2ee3
PatoProgramador <skpates18@gmail.com>
2024-05-30 17:35:17 -0500

    DB config, command line runner

--- src/main/java/com/literaturaPhatos/literaturaAPI/LiteraturaApiApplication.java
+++ src/main/java/com/literaturaPhatos/literaturaAPI/LiteraturaApiApplication.java
@@ -1,13 +1,17 @@
 package com.literaturaPhatos.literaturaAPI;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraturaApiApplication {
+public class LiteraturaApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraturaApiApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Corriendooooooo");
+	}
 }
